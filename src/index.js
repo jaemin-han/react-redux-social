5df547d88d32e95705fef05b335844b407b8d442
Jaemin Han <jaemin.han@nyu.edu>
2017-02-23 20:59:13 -0500

    components and index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React    from 'react';
-import ReactDOM from 'react-dom';
-import App      from './components/App.jsx';
+import React from 'react';
+import { render } from 'react-dom';
 
-// mount our App at #container
-ReactDOM.render(<App />, document.querySelector('#root-container'));
+// Import css
+import css from './styles/style.styl';
+
+// Import from components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+
+render(router, document.getElementById('root'));
