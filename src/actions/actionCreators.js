fc635912b0e97cde20bf59a8d42bebaf24632fd4
Jaemin Han <jaemin.han@nyu.edu>
2017-02-26 20:39:11 -0500

    actions file in progress

---
+++ src/actions/actionCreators.js
@@ -0,0 +1,26 @@
+// Increment
+function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+// Add comment
+function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+// Remove comment
+function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
